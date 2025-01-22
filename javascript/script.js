83d0bd91be19760f6aaef31912412c753f704e00
Eterseis <148352159+eterseis@users.noreply.github.com>
2025-01-21 22:04:14 -0300

    fix: rewrite all the javascript logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,83 +1,143 @@
-const operators = ["+", "-", "*", "/", "%", "+/-", "C", "="];
-
-let aArray = [];
-let arithmetic_operator = null;
-let bArray = [];
+let a = [];
+let b = [];
+let operator = null;
 
 const output = document.querySelector(".output");
 
 const input = document.querySelector(".input");
-input.addEventListener("click", function (e) {
-  if (operators.indexOf(e.target.textContent) === -1) {
-    if (arithmetic_operator === null) {
-      aArray.push(e.target.textContent);
-      output.textContent = aArray.join("");
-    } else {
-      bArray.push(e.target.textContent);
-      output.textContent = bArray.join("");
-    }
-  } else if (operators.slice(0, 5).indexOf(e.target.textContent) != -1) {
-    arithmetic_operator = e.target.textContent;
+input.addEventListener("click", (e) => {
+  if (!(e.target.classList[0] == "column")) return;
+  if (e.target.id === "clearAll" && a.length > 0) {
+    operator = clearAll(a, b, output);
   }
-  if (
-    (e.target.textContent === "=" && arithmetic_operator != null) ||
-    e.target.textContent == "%"
-  ) {
-    switch (arithmetic_operator) {
-      case "+":
-        output.textContent = add(aArray, bArray);
-        break;
-      case "-":
-        output.textContent = subtraction(aArray, bArray);
-        break;
-      case "*":
-        output.textContent = multiply(aArray, bArray);
-        break;
-      case "/":
-        output.textContent = divide(aArray, bArray);
-        break;
-      case "%":
-        output.textContent = percentage(aArray);
-        break;
-    }
-    clear();
+  if (e.target.id === "changeSign" && a.length > 0) {
+    changeSign(a, b, operator, output);
+  }
+
+  if (e.target.classList.length > 1 && a.length > 0) {
+    operator = e.target.textContent;
+  }
+  if (!isValid(e.target.id, a, b)) {
+    getNumbers(e.target, output, operator, a, b);
+  } else {
+    a = String(calculate(a, b, getCallback(operator))).split("");
+    b.length = 0;
+    operator = null;
+    output.textContent = a.join("");
   }
 });
 
-function validate(arr) {
-  return typeof Number(arr.join("")) === "number" ? true : false;
+function getNumbers(target, output, operator, a = [], b = []) {
+  //------------------Validate-----------------//
+  if (!(Number("0" + target.textContent) >= 0)) return;
+  if (target.textContent === "." && a.includes(".") && operator === null)
+    return;
+  if (target.textContent === "." && b.includes(".") && operator !== null)
+    return;
+  if (target.textContent === "0" && a[0] === "0" && a.indexOf(".") === -1)
+    return;
+  if (target.textContent === "0" && b[0] === "0" && b.indexOf(".") === -1)
+    return;
+  //------------------------------------------//
+
+  if (operator === null) {
+    if (target.textContent === "." && a.length === 0) {
+      a.push("0");
+      a.push(".");
+    } else {
+      a.push(target.textContent);
+    }
+    output.textContent = a.join("");
+  } else {
+    if (target.textContent === "." && b.length === 0) {
+      b.push("0");
+      b.push(".");
+    } else {
+      b.push(target.textContent);
+    }
+    output.textContent = b.join("");
+  }
 }
-function add(arr, arr2) {
-  if (!(validate(arr) && validate(arr2))) return "not a number";
-  let a = Number(arr.join(""));
-  let b = Number(arr2.join(""));
+
+function calculate(a = [], b = [], callback) {
+  let n1 = Number(a.join(""));
+  let n2 = Number(b.join(""));
+  if (typeof (n1 + n2) !== "number") return;
+  return callback(n1, n2);
+}
+
+function getCallback(operator) {
+  switch (operator) {
+    case "+":
+      return add;
+    case "-":
+      return subtract;
+    case "*":
+      return multiply;
+    case "/":
+      return divide;
+    case "%":
+      return percentage;
+  }
+}
+
+function clearAll(a = [], b = [], output) {
+  a.length = 0;
+  b.length = 0;
+  output.textContent = 0;
+  return null;
+}
+
+function changeSign(a = [], b = [], operator, output) {
+  if (a[0] === "0" || b[0] === "0") return;
+  if (operator === null) {
+    if (a[0] === "-") {
+      a.shift();
+      output.textContent = a.join("");
+    } else {
+      a.unshift("-");
+      output.textContent = a.join("");
+    }
+  } else {
+    if (b[0] === "-") {
+      b.shift();
+      output.textContent = b.join("");
+    } else {
+      b.unshift("-");
+      output.textContent = b.join("");
+    }
+  }
+}
+
+function isValid(btn, a = [], b = []) {
+  if (btn === "equals"){
+    if (a.length > 0 && b.length > 0){
+      return true;
+    }
+  }
+
+  if (btn === "percentage"){
+    if (a.length > 0) return true;
+  }
+  return false;
+}
+
+function add(a, b) {
   return a + b;
 }
-function subtraction(arr, arr2) {
-  if (!(validate(arr) && validate(arr2))) return "not a number";
-  let a = Number(arr.join(""));
-  let b = Number(arr2.join(""));
+
+function subtract(a, b) {
   return a - b;
 }
-function multiply(arr, arr2) {
-  if (!(validate(arr) && validate(arr2))) return "not a number";
-  let a = Number(arr.join(""));
-  let b = Number(arr2.join(""));
+
+function multiply(a, b) {
   return a * b;
 }
-function divide(arr, arr2) {
-  if (!(validate(arr) && validate(arr2))) return "not a number";
-  let a = Number(arr.join(""));
-  let b = Number(arr2.join(""));
+
+function divide(a, b) {
   return a / b;
 }
-function percentage(arr) {
-  if (!validate(arr)) return "not a number";
-  let a = Number(arr.join(""));
-  return (a * 100) / 100 / 100;
-}
-function clear() {
-  aArray = [];
-  bArray = [];
-  arithmetic_operator = null;
+
+function percentage(a) {
+  return a / 100;
 }
